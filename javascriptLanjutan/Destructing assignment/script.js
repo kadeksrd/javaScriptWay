64113ad007b5e8238289b27aaf5658b6e8950ba0
kadeksrd <kadeksuryard13@gmail.com>
2022-07-04 17:09:52 +0700

    updates destructuring array [1]

--- javascriptLanjutan/Destructing assignment/script.js
+++ javascriptLanjutan/Destructing assignment/script.js
@@ -33,8 +33,58 @@
     // cukup kosongin aja variabelnya  
     // misal pgn ambil array nama dan saya
 
-    const [ ,satu,dua, ] = perkenalan
-    console.log(dua);
+    // const [ ,satu,dua, ] = perkenalan
+    // console.log(dua);
 
     // swap item 
     // menukar isi array
+
+        // let a = 1;
+        // let b = 2;
+
+        // console.log(a);
+        // console.log(b);
+
+        // [a,b] = [b,a];
+
+        // console.log(a)
+        // console.log(b)
+    
+    // return value pada function 
+
+    function coba () {
+        return [1,2];
+    }
+
+    // cara array biasa
+    
+    // const test = coba();
+
+    // console.log(test[0], test[1]);
+
+    // cara destructuring array 
+
+    // const [a,b] = coba()
+    // console.log(a,b)
+
+    // maka hasilnya sama cuma lebih simple sih destructuring
+
+// Rest Parameter untuk Destructur array
+
+//cara biasa 
+
+// const [a,b] = [1,2,3,4,5];
+
+//cara biasa 
+// console.log(a,b,c)
+
+// 1. menggunakan cara biasa kita harus buat variabel baru buat ambil array nya
+// 2. maka gunakan cara rest parameter biar semua array keambil tanpa buat variabel baru
+
+// cara rest parameter
+
+const [a,...b] = [1,2,3,4,5,6,7,8];
+
+console.log(a,b);
+
+// maka akan kepanggil semua walaupun yang dipanggil dalam bentuk array 
