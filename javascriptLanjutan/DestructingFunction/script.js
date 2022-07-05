79fa997bb0045c1ef2e780029870d08c1515a004
kadeksrd <kadeksuryard13@gmail.com>
2022-07-05 15:34:32 +0700

    update baru

--- javascriptLanjutan/DestructingFunction/script.js
+++ javascriptLanjutan/DestructingFunction/script.js
@@ -12,12 +12,12 @@ const all = sumMultiples(2,3);
     
 // cara manggil function yang dibutuhkan 
     // dengan membuat variabel baru
-        // const jumlah = sumMultiples(2,3)[0];
-        // const kali = sumMultiples(2,3)[1];
+        const jumlah = sumMultiples(2,3)[0];
+        const kali = sumMultiples(2,3)[1];
 
     // dengan destructuring function 
 
-        // const[jumlah,kali] = sumMultiples(2,3);
+        const[jumlah,kali] = sumMultiples(2,3);
         // lebih simple 
 
 
